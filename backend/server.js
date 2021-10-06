99c8c7ce4bcec823f5f647072837d4c49fa9b0dc
Emin Garača <emingaracaeme@hotmail.com>
2021-10-06 11:54:48 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const products = require('./data/products')
+const connectDB = require('./config/db')
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Api is running...')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+})
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
